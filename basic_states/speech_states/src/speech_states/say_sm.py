12a93d469c5bdf5e1b1e41f006397112277916c1
Sergi Xavier Ubach Pallas <sxubach@gmail.com>
2014-02-22 18:49:34 +0100

    adding say state machine

---
+++ basic_states/speech_states/src/speech_states/say_sm.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 22 12:00:00 2013
+
+@author: sampfeiffer
+"""
+
+import rospy
+import smach
+import smach_ros
+import actionlib
+#from smach_ros import SimpleActionState, ServiceState
+
+
+from say import text_to_say
+
+
+class DummyStateMachine(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['succeeded'], output_keys=[])
+
+    def execute(self, userdata):
+        print "Dummy state to launch real State Machine"
+        rospy.sleep(1) # in seconds
+
+        return 'succeeded'
+
+
+def main():
+    rospy.init_node('sm_example_sm_pkg')
+
+    sm = smach.StateMachine(outcomes=['succeeded', 'preempted', 'aborted'])
+
+    with sm:
+
+        # Using this state to wait and to initialize stuff if necessary (fill up input/output keys for example)
+        smach.StateMachine.add(
+            'dummy_state',
+            DummyStateMachine(),
+            transitions={'succeeded': 'SaySM'})
+
+        sm.userdata.tts_text = "I'm working"
+        sm.userdata.tts_wait_before_speaking = 0
+        smach.StateMachine.add(
+            'SaySM',
+            text_to_say(),
+            transitions={'succeeded': 'succeeded', 'aborted': 'aborted'})
+
+    # This is for the smach_viewer so we can see what is happening, rosrun smach_viewer smach_viewer.py it's cool!
+    sis = smach_ros.IntrospectionServer(
+        'sm_example_sm_pkg_introspection', sm, '/SM_ROOT')
+    sis.start()
+
+    sm.execute()
+
+    rospy.spin()
+    sis.stop()
+
+
+if __name__ == '__main__':
+    main()
