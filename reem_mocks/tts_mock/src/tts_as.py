84bf4e5b90c4e2dd7d7586a32f68b1dc2e6ac583
Sergi Xavier Ubach Pallas <sxubach@gmail.com>
2014-02-26 13:33:01 +0100

    implemented fake mock server of tts

---
+++ reem_mocks/tts_mock/src/tts_as.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+import rospy
+import actionlib
+
+from text_to_speech.msg import SoundAction, SoundGoal, SoundResult 
+
+class TtsActionServer:
+  def __init__(self):
+    self.server = actionlib.SimpleActionServer('/sound', SoundAction, self.execute, False)
+    self.server.start()
+    rospy.loginfo("ActionServer running, waiting for goals")
+
+  def execute(self, goal):
+    # Do lots of awesome groundbreaking robot stuff here
+    rospy.sleep(goal.wait_before_speaking)
+    rospy.loginfo(goal.lang_id)
+    rospy.loginfo(goal.text)
+    result = SoundResult()
+    result.text = goal.text
+        
+    self.server.set_succeeded(result)
+
+if __name__ == '__main__':
+  rospy.init_node('tts_as')
+  rospy.loginfo("Initializing tts_as")
+  server = TtsActionServer()
+  rospy.spin()
