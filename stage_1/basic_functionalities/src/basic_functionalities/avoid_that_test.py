bafa544ac000f97e604c759e61dbbc1181a19102
roger boldu <roger.boldu@gmail.com>
2014-02-26 15:15:36 +0100

    update of alpha avoid_that

---
+++ stage_1/basic_functionalities/src/basic_functionalities/avoid_that_test.py
@@ -0,0 +1,73 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 22 12:00:00 2013
+
+@author: sampfeiffer
+"""
+
+import rospy
+import smach
+import smach_ros
+import actionlib
+from smach_ros import SimpleActionState, ServiceState
+
+#from robot_inspection_sm import RobotInspectionSM
+from avoid_that_sm import Avoid_That
+
+
+class DummyStateMachine(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['succeeded'], output_keys=[])
+
+    def execute(self, userdata):
+        print "Test state of Avoid That"
+        #rospy.sleep(1) # in seconds
+
+        return 'succeeded'
+
+class Avoid_That_error(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['aborted'],input_keys=['standard_error'], output_keys=[])
+
+    def execute(self, userdata):
+	print 'info of aborted Avoid That'
+        print userdata.standard_error
+        return 'aborted'
+
+def main():
+    rospy.init_node('avoid_that_test')
+
+    sm = smach.StateMachine(outcomes=['succeeded', 'preempted', 'aborted'])
+
+    with sm:
+
+        # Using this state to wait and to initialize stuff if necessary (fill up input/output keys for example)
+        smach.StateMachine.add(
+            'dummy_state',
+            DummyStateMachine(),
+            transitions={'succeeded': 'RobotInspectionSM'})
+
+        smach.StateMachine.add(
+            'avoid_that_sm',
+            Avoid_That(),
+            transitions={'succeeded': 'succeeded', 'aborted': 'aborted_info'})
+
+        smach.StateMachine.add(
+            'aborted_info',
+            Avoid_That_error(),
+            transitions={'succeeded': 'aborted', 'aborted': 'aborted'})
+
+    # This is for the smach_viewer so we can see what is happening, rosrun smach_viewer smach_viewer.py it's cool!
+    sis = smach_ros.IntrospectionServer(
+        'avoid_that_test', sm, '/SM_ROOT')
+    sis.start()
+
+    sm.execute()
+
+    rospy.spin()
+    sis.stop()
+
+
+if __name__ == '__main__':
+    main()
