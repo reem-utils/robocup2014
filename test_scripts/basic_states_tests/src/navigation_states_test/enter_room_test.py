09baaa8629b4208a25fcc29f553596776b0e91cb
Cristina <crsaintc8@gmail.com>
2014-03-27 17:16:04 +0100

    Some modifications in navigation_states

---
+++ test_scripts/basic_states_tests/src/navigation_states_test/enter_room_test.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+27/03/2014
+
+@author: Cristina De Saint Germain
+"""
+
+import rospy
+import smach
+import smach_ros
+
+from navigation_states.enter_room import EnterRoomSM
+
+def main():
+    rospy.init_node('enter_room_test')
+    
+
+    sm = smach.StateMachine(outcomes=['succeeded', 'preempted', 'aborted'])
+    
+    with sm:
+
+        sm.userdata.nav_to_poi_name = "find_me"
+        
+        smach.StateMachine.add(
+            'enter_room_test',
+            EnterRoomSM(),
+            transitions={'succeeded': 'succeeded','aborted' : 'aborted'})
+        
+    # This is for the smach_viewer so we can see what is happening, rosrun smach_viewer smach_viewer.py it's cool!
+    sis = smach_ros.IntrospectionServer(
+        'enter_room_introspection', sm, '/enterRoom_ROOT')
+    sis.start()
+
+    sm.execute()
+
+    rospy.spin()
+    sis.stop()
+
+
+if __name__ == '__main__':
+    main()
