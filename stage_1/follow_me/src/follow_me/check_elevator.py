86594bc17059395d2dd021f18fbc964b519a334e
roger boldu <roger.boldu@gmail.com>
2014-03-30 20:42:22 +0200

    version beta of follow_me and check elevator

--- stage_1/follow_me/src/follow_me/check_elevator.py
+++ stage_1/follow_me/src/follow_me/check_elevator.py
@@ -34,6 +34,7 @@ from util_states.topic_reader import topic_reader
 from bzrlib import switch
 from nose import case
 from navigation_states.get_current_robot_pose import get_current_robot_pose
+from geometry_msgs.msg import PoseWithCovarianceStamped
 
 ultraNow=0
 
@@ -42,7 +43,7 @@ class init_var(smach.State):
         smach.State.__init__(self, outcomes=['succeeded'],output_keys=['ultraSound','ultraNow'])
     def execute(self, userdata):
             #userdata.ultra_sound_filtered=22
-            rospy.sleep(1)
+          
             rospy.loginfo("i'm in dummy init var")
             userdata.ultraSound=[]
             userdata.ultraNow=0
@@ -61,7 +62,41 @@ class pinta_ultra_sound(smach.State):
         
         
         
+     
+class process_pose(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['succeeded'],
+                             input_keys=['pose_1','pose_2'],
+                             output_keys=['pose_status'])
+    def execute(self, userdata):
+            #userdata.ultra_sound_filtered=22
+        aux=userdata.pose_1
+        aux1=userdata.pose_2
+        #aux1=PoseWithCovarianceStamped()
+        min_difrerence_x = 0.05
+        min_difrerence_y = 0.05
+        userdata.pose_status=False
         
+        if (aux.pose.position.x+min_difrerence_x)>aux1.pose.position.x and (aux.pose.position.x-min_difrerence_x)<aux1.pose.position.x :
+            if (aux.pose.position.y+min_difrerence_y)>aux1.pose.position.y and (aux.pose.position.y-min_difrerence_y)<aux1.pose.position.y :
+                userdata.pose_status=True
+            else :
+                userdata.pose_status=False
+        else :
+            userdata.pose_status=False
+        rospy.loginfo(OKGREEN)
+        rospy.loginfo("//////////////////////////////////POSE/////////////////////////////")
+        
+        rospy.loginfo(str(aux.pose.position.x))
+        rospy.loginfo(str(aux.pose.position.y))
+        rospy.loginfo(str(aux1.pose.position.x))
+        rospy.loginfo(str(aux1.pose.position.y))
+        rospy.loginfo(ENDC)
+        return 'succeeded'
+
+
+
+   
 class process_laser(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['succeeded'],
@@ -76,25 +111,24 @@ class process_laser(smach.State):
             userdata.Laser_status=True
         else :
             userdata.Laser_status=False
-            rospy.loginfo(OKGREEN)
-            rospy.loginfo("//////////////////////////////////LASER/////////////////////////////")
-            
-            rospy.loginfo(str(userdata.Laser_msg.ranges[0]))
-            rospy.loginfo(str(userdata.Laser_msg.ranges[length-1]))
-            rospy.loginfo(ENDC)
+        rospy.loginfo(OKGREEN)
+        rospy.loginfo("//////////////////////////////////LASER/////////////////////////////")
+        
+        rospy.loginfo(str(userdata.Laser_msg.ranges[0]))
+        rospy.loginfo(str(userdata.Laser_msg.ranges[length-1]))
+        rospy.loginfo(ENDC)
         return 'succeeded'
 
 
 class process_info(smach.State):
     def __init__(self):
         smach.State.__init__(self,outcomes=['elevator','no_elevator'],
-                             input_keys=['Laser_status','pose_2','pose_1','ultraSound_status'])
+                             input_keys=['Laser_status','pose_status','ultraSound_status'])
     def execute(self,userdata):
         rospy.loginfo(OKGREEN)
-        rospy.loginfo(str(userdata.Laser_status))
-        rospy.loginfo(str(userdata.pose_2))
-        rospy.loginfo(str(userdata.pose_1))
-        rospy.loginfo(str(userdata.ultraSound_status))
+        rospy.loginfo("Pose say     :"+str(userdata.pose_status))
+        rospy.loginfo("Ultra sound say:    "+str(userdata.ultraSound_status))
+        rospy.loginfo("Laser say:    "+str(userdata.Laser_status))
         rospy.loginfo(ENDC)
         return 'no_elevator'
      
@@ -117,6 +151,8 @@ class get_ultra_sound(smach.State):
         
         while Finish==False:
             aux = rospy.wait_for_message('/sonar_base', Range, 60)
+            pep=Range()
+            pep.range
             
             
             if ultra[0]==False :       
@@ -142,7 +178,7 @@ class get_ultra_sound(smach.State):
             if ultra[0]==True and ultra[1]==True and ultra[2]==True and ultra[3]==True :
                 Finish = True
         
-        if ultraSound[0]<distance and ultraSound[1]<distance and ultraSound[2]<distance and ultraSound[3]<distance :
+        if ultraSound[0].range<distance and ultraSound[1].range<distance and ultraSound[2].range<distance and ultraSound[3].range<distance :
             userdata.ultraSound_status=True
         else :
             userdata.ultraSound_status=False
@@ -154,7 +190,7 @@ class get_ultra_sound(smach.State):
             rospy.loginfo(str(ultraSound[3]))
             rospy.loginfo(ENDC)
         return 'succeeded'
-     
+'''     
 class get_ultra_sound3(smach.State):
      
     def __init__(self):
@@ -200,12 +236,12 @@ class get_ultra_sound3(smach.State):
             userdata.ultraSound_status=False
         return 'succeeded'
 
-        
+'''       
                     
                                   
     
 class CheckElevator(smach.StateMachine):
-    def __init__(self, distToHuman=0.9):
+    def __init__(self):
         smach.StateMachine.__init__(self,
                                     ['succeeded', 'preempted', 'aborted'])
         with self:
@@ -244,9 +280,13 @@ class CheckElevator(smach.StateMachine):
             
             smach.StateMachine.add('GET_CURRENT_POSE_2',
                                    get_current_robot_pose(),
-                      transitions={'succeeded': 'PROCESS_INFO'},
+                      transitions={'succeeded': 'PROCESS_POSE'},
                       remapping={'current_robot_pose':'pose_2'})
             
+            smach.StateMachine.add('PROCESS_POSE',
+                                   process_pose(),
+                      transitions={'succeeded':'PROCESS_INFO'})
+                       
             smach.StateMachine.add('PROCESS_INFO',
                                    process_info(),
                       transitions={'elevator':'succeeded','no_elevator':'INIT_VAR'})
