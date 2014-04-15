787b8f5e15a7935a29416f035e6859e1cd12f0b5
changlongzj <chlzr@hotmail.com>
2014-04-15 19:47:02 +0200

    some changes

--- stage_1/robozoo/src/robozoo/robozoo_sm.py
+++ stage_1/robozoo/src/robozoo/robozoo_sm.py
@@ -13,7 +13,7 @@
 import rospy
 import smach
 
-
+from manipulation_states.play_motion_sm import play_motion_sm
 
 # Some color codes for prints, from http://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python
 ENDC = '\033[0m'
@@ -61,106 +61,107 @@ class RoboZooSM(smach.StateMachine):
             # Do the Macarena Dance
             smach.StateMachine.add(
                 'do_macarena',
-                DummyStateMachine(),
+                #DummyStateMachine(),
+                play_motion_sm('macarena'),
                 transitions={'succeeded': 'do_YMCA', 'aborted': 'aborted', 
                 'preempted': 'preempted'}) 
 
             # Do the YMCA Dance
             smach.StateMachine.add(
                 'do_YMCA',
-                DummyStateMachine(),
+                play_motion_sm('ymca'),
                 transitions={'succeeded': 'do_robot', 'aborted': 'aborted', 
                 'preempted': 'preempted'})    
 
             # Do the Robot Dance
             smach.StateMachine.add(
                 'do_robot',
-                DummyStateMachine(),
-                transitions={'succeeded': 'prepare_fetch_and_carry', 'aborted': 'aborted', 
+                play_motion_sm('robot'),
+                transitions={'succeeded': 'succeed', 'aborted': 'aborted', 
                 'preempted': 'preempted'}) 
-
-            # Prepare the poi for Fetch and Carry
-            smach.StateMachine.add(
-                'do_jokes',
-                prepare_fetch_and_carry(),
-                transitions={'succeeded': 'go_fetch_and_carry', 'aborted': 'aborted', 
-                'preempted': 'preempted'})  
-
-            # Go to fetch and carry
-            smach.StateMachine.add(
-                'go_fetch_and_carry',
-                nav_to_poi(),
-                transitions={'succeeded': 'do_fetch_and_carry', 'aborted': 'aborted', 
-                'preempted': 'preempted'})    
-
-            # Do fetch and carry
-            smach.StateMachine.add(
-                'do_fetch_and_carry',
-                FetchAndCarrySM(),
-                transitions={'succeeded': 'prepare_find_me', 'aborted': 'aborted', 
-                'preempted': 'preempted'}) 
-
-            # Prepare the poi for find me
-            smach.StateMachine.add(
-                'prepare_find_me',
-                prepare_find_me(),
-                transitions={'succeeded': 'go_find_me', 'aborted': 'aborted', 
-                'preempted': 'preempted'})  
-
-            # Go to find me
-            smach.StateMachine.add(
-                'go_find_me',
-                nav_to_poi(),
-                transitions={'succeeded': 'do_find_me', 'aborted': 'aborted', 
-                'preempted': 'preempted'})    
-
-            # Do find me
-            smach.StateMachine.add(
-                'do_find_me',
-                FindMeSM(),
-                transitions={'succeeded': 'prepare_avoid_that', 'aborted': 'aborted', 
-                'preempted': 'preempted'}) 
-
-            # Prepare the poi for avoid that
-            smach.StateMachine.add(
-                'prepare_avoid_that',
-                prepare_avoid_that(),
-                transitions={'succeeded': 'go_avoid_that', 'aborted': 'aborted', 
-                'preempted': 'preempted'})  
-
-            # Go to avoid that
-            smach.StateMachine.add(
-                'go_avoid_that',
-                nav_to_poi(),
-                transitions={'succeeded': 'do_avoid_that', 'aborted': 'aborted', 
-                'preempted': 'preempted'})    
-
-            # Do avoid that
-            smach.StateMachine.add(
-                'do_avoid_that',
-                Avoid_That(),
-                transitions={'succeeded': 'prepare_what_did_you_say', 'aborted': 'aborted', 
-                'preempted': 'preempted'}) 
-
-            # Prepare the poi for what did you say
-            smach.StateMachine.add(
-                'prepare_what_did_you_say',
-                prepare_what_did_you_say(),
-                transitions={'succeeded': 'go_what_did_you_say', 'aborted': 'aborted', 
-                'preempted': 'preempted'})  
-
-            # Go to what did you say
-            smach.StateMachine.add(
-                'go_what_did_you_say',
-                nav_to_poi(),
-                transitions={'succeeded': 'do_what_did_you_say', 'aborted': 'aborted', 
-                'preempted': 'preempted'})    
-
-            # Do what did you say
-            smach.StateMachine.add(
-                'do_what_did_you_say',
-                WhatSaySM(),
-                transitions={'succeeded': 'succeeded', 'aborted': 'aborted', 
-                'preempted': 'preempted'}) 
+# 
+#             # Prepare the poi for Fetch and Carry
+#             smach.StateMachine.add(
+#                 'do_jokes',
+#                 prepare_fetch_and_carry(),
+#                 transitions={'succeeded': 'go_fetch_and_carry', 'aborted': 'aborted', 
+#                 'preempted': 'preempted'})  
+# 
+#             # Go to fetch and carry
+#             smach.StateMachine.add(
+#                 'go_fetch_and_carry',
+#                 nav_to_poi(),
+#                 transitions={'succeeded': 'do_fetch_and_carry', 'aborted': 'aborted', 
+#                 'preempted': 'preempted'})    
+# 
+#             # Do fetch and carry
+#             smach.StateMachine.add(
+#                 'do_fetch_and_carry',
+#                 FetchAndCarrySM(),
+#                 transitions={'succeeded': 'prepare_find_me', 'aborted': 'aborted', 
+#                 'preempted': 'preempted'}) 
+# 
+#             # Prepare the poi for find me
+#             smach.StateMachine.add(
+#                 'prepare_find_me',
+#                 prepare_find_me(),
+#                 transitions={'succeeded': 'go_find_me', 'aborted': 'aborted', 
+#                 'preempted': 'preempted'})  
+# 
+#             # Go to find me
+#             smach.StateMachine.add(
+#                 'go_find_me',
+#                 nav_to_poi(),
+#                 transitions={'succeeded': 'do_find_me', 'aborted': 'aborted', 
+#                 'preempted': 'preempted'})    
+# 
+#             # Do find me
+#             smach.StateMachine.add(
+#                 'do_find_me',
+#                 FindMeSM(),
+#                 transitions={'succeeded': 'prepare_avoid_that', 'aborted': 'aborted', 
+#                 'preempted': 'preempted'}) 
+# 
+#             # Prepare the poi for avoid that
+#             smach.StateMachine.add(
+#                 'prepare_avoid_that',
+#                 prepare_avoid_that(),
+#                 transitions={'succeeded': 'go_avoid_that', 'aborted': 'aborted', 
+#                 'preempted': 'preempted'})  
+# 
+#             # Go to avoid that
+#             smach.StateMachine.add(
+#                 'go_avoid_that',
+#                 nav_to_poi(),
+#                 transitions={'succeeded': 'do_avoid_that', 'aborted': 'aborted', 
+#                 'preempted': 'preempted'})    
+# 
+#             # Do avoid that
+#             smach.StateMachine.add(
+#                 'do_avoid_that',
+#                 Avoid_That(),
+#                 transitions={'succeeded': 'prepare_what_did_you_say', 'aborted': 'aborted', 
+#                 'preempted': 'preempted'}) 
+# 
+#             # Prepare the poi for what did you say
+#             smach.StateMachine.add(
+#                 'prepare_what_did_you_say',
+#                 prepare_what_did_you_say(),
+#                 transitions={'succeeded': 'go_what_did_you_say', 'aborted': 'aborted', 
+#                 'preempted': 'preempted'})  
+# 
+#             # Go to what did you say
+#             smach.StateMachine.add(
+#                 'go_what_did_you_say',
+#                 nav_to_poi(),
+#                 transitions={'succeeded': 'do_what_did_you_say', 'aborted': 'aborted', 
+#                 'preempted': 'preempted'})    
+# 
+#             # Do what did you say
+#             smach.StateMachine.add(
+#                 'do_what_did_you_say',
+#                 WhatSaySM(),
+#                 transitions={'succeeded': 'succeeded', 'aborted': 'aborted', 
+#                 'preempted': 'preempted'}) 
 
 
