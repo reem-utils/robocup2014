69f3e835e888bed61dc0555aea35a4764ef7e4da
changlongzj <chlzr@hotmail.com>
2014-04-15 18:05:22 +0200

    Updating Play_Motion_SM for new Play_Motion_Msgs

--- basic_states/manipulation_states/src/manipulation_states/play_motion_sm.py
+++ basic_states/manipulation_states/src/manipulation_states/play_motion_sm.py
@@ -30,7 +30,7 @@ class createPlayMotionGoal(smach.State):
         play_goal = PlayMotionGoal()
         play_goal.motion_name = userdata.manip_motion_to_play
         #play_goal.reach_time.secs = userdata.manip_time_to_play
-        play_goal.skip_planning = false
+        play_goal.skip_planning = False
 	userdata.play_motion_sm_goal = play_goal
         
         return 'succeeded'   
