060fcf7c6ec0b49eb63aa09bf873b145fe3f8799
changlongzj <chlzr@hotmail.com>
2014-04-26 19:07:04 +0200

    Updating Check_exists_yaml

--- basic_states/util_states/src/util_states/check_exists_yaml.py
+++ basic_states/util_states/src/util_states/check_exists_yaml.py
@@ -3,12 +3,27 @@ import string
 import os
 import rospkg
 import sys
-"""
-    Checks if objects in the yaml file are included in the roboList from the Robocup
-"""
-def check_yaml_compare(yamlFilePath, file_to_compare):
-    f = open(yamlFilePath, 'r')
-    f_comp = open(file_to_compare, 'r')
+
+def check_yaml_compare(yamlFilePath, file_to_compare, package_folder):
+    """
+    Checks if objects from the Robocup file are included in the yaml file.
+    
+    Parameters:
+    @param: yamlFilePath: the name of the .yaml File (without .yaml)
+    @param: file_to_comapre: the name of the file to compare, usually roboList (should be included in the /config folder)
+    @param: package_folder : the name of the package
+    
+    Returns:
+    @return True: if the object exists
+    @return: False: if the object Does Not exist
+    """
+    
+    rospack_instance = rospkg.RosPack()
+    file_path = rospack_instance.get_path(package_folder)
+    filePath = os.path.expanduser(file_path) + "/config/"
+    
+    f = open(filePath + yamlFilePath + ".yaml", 'r')
+    f_comp = open(filePath + file_to_compare, 'r')
     
     end = False
     #Reading the file to compare with yaml
@@ -61,17 +76,15 @@ def main():
     if(len(sys.argv) > 3):
         yamlName = sys.argv[1]
         file_to_compare_name = sys.argv[2]
-        package_folder = sys.argv[3] 
+        package_folder_name = sys.argv[3] 
+        print "SYS"
     else:
         yamlName = 'pois_cocktail_party'
         file_to_compare_name = 'roboList'
-        package_folder = "cocktail_party"
+        package_folder_name = "cocktail_party"
     
     
-    rospack_instance = rospkg.RosPack()
-    file_path = rospack_instance.get_path(package_folder)
-    filePath = os.path.expanduser(file_path) + "/config/"
-    check_yaml_compare(filePath + yamlName + ".yaml", filePath + file_to_compare_name)
+    check_yaml_compare(yamlName,file_to_compare_name, package_folder_name)
     
 if __name__ == '__main__':
     main()    
