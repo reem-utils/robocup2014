686f074ebe85514ac92d745d84f6a5ca9aed49a5
Sammy Pfeiffer <sam.pfeiffer@pal-robotics.com>
2014-06-06 11:18:13 +0200

    Updating Search for rooms

--- stage_1/emergency_situation/src/emergency_situation/Search_Emergency_Wave_Room_Change_SM.py
+++ stage_1/emergency_situation/src/emergency_situation/Search_Emergency_Wave_Room_Change_SM.py
@@ -14,8 +14,6 @@ from navigation_states.nav_to_poi import nav_to_poi
 from speech_states.say import text_to_say
 from gesture_states.search_wave_sm import Search_Wave_SM
 
-#from emergency_situation.GeneratePDF_State import GeneratePDF_State
-
 # Some color codes for prints, from http://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python
 ENDC = '\033[0m'
 FAIL = '\033[91m'
@@ -23,18 +21,31 @@ OKGREEN = '\033[92m'
 possible_pois = []
 
 class Prepare_Data(smach.State):
+    """
+    State Machine that prepares the data, concretely the pois, for the 'Search Emergency Wave Room".
+    It lists all the possible pois for the robot to search the person in emergency
+    """
     def __init__(self):
         smach.State.__init__(self, outcomes=['succeeded'], 
-            input_keys=[],
+            input_keys=['possible_pois'],
             output_keys=['possible_pois']) #todo: i have to delate de output_key
 
     def execute(self, userdata):
         aux_pois = rospy.get_param("/emergency_possible_pois/pois/emergency/")
+        for poi_aux in aux_pois.keys():
+            str(poi_aux).replace('_',' ')
+            userdata.possible_pois.append(poi_aux)
+            
         userdata.possible_pois = aux_pois.keys()
+        
         rospy.sleep(1)
         return 'succeeded'
 
 class Select_Possible_Poi(smach.State):
+    """
+    From the userdata, it selects a poi from the list, in order to get the robot to that location
+    """
+    
     def __init__(self):
         smach.State.__init__(self, outcomes=['succeeded', 'finished_searching'], 
                                 input_keys=['possible_pois', 'n_item'],
@@ -45,13 +56,14 @@ class Select_Possible_Poi(smach.State):
         print "POIS:: ----- " + str(userdata.possible_pois)
         
         aux_possible = userdata.possible_pois.pop()
+        
         userdata.nav_to_poi_name_possible = aux_possible
         rospy.loginfo("Possible Poi:: " + str(aux_possible))
         userdata.n_item = userdata.n_item + 1
         
         print "Length of List:: " + str(len(userdata.possible_pois))
         
-        if userdata.n_item == len(userdata.possible_pois):
+        if len(userdata.possible_pois) == 0:
             return 'finished_searching'
         
         rospy.loginfo("To Suceeded")
@@ -91,6 +103,9 @@ class Search_Emergency_Wave_Room_Change(smach.StateMachine):
             self.userdata.tts_wait_before_speaking = 0
             self.userdata.possible_pois = []
             self.userdata.n_item = 0
+            
+            
+            
             smach.StateMachine.add(
                 'Prepare_Data',
                 Prepare_Data(),
