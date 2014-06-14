cf584cb5b29e8cfb99ba31b34507297fb0b61bfc
changlongzj <chlzr@hotmail.com>
2014-06-14 19:01:10 +0200

    Updating Emergency Finding Ambulance, it seems to work 95% of the time

--- stage_1/emergency_situation/src/emergency_situation/search_ambulance.py
+++ stage_1/emergency_situation/src/emergency_situation/search_ambulance.py
@@ -11,11 +11,28 @@ import smach
 from navigation_states.nav_to_poi import nav_to_poi
 from manipulation_states.move_head import move_head
 from util_states.timeout import TimeOut
-from face_states.recognize_face import recognize_face
+from face_states.recognize_face import recognize_face_concurrent
 from speech_states.say import text_to_say
 from manipulation_states.move_head_form import move_head_form
 
+import tf
+import numpy
+import copy
+import tf.transformations as TT
+from std_msgs.msg import Header
+from geometry_msgs.msg import PoseStamped, Pose, PointStamped, Point, Quaternion
+from util_states.topic_reader import topic_reader
+from pal_vision_msgs.msg import Gesture
+from face_states.detect_faces import detect_face
+from util_states.math_utils import *
+from gesture_states.wave_detection_sm import WaveDetection
 
+GESTURE_TOPIC = '/head_mount_xtion/gestures'
+final_frame_id = 'base_link'
+PUB_1_TOPIC = '/given_pose'
+PUB_2_TOPIC = '/transformed_pose'
+
+NUMBER_OF_HEAD_POSES = 3
 # Some color codes for prints, from http://stackoverflow.com/questions/287871/print-in-terminal-with-colors-using-python
 ENDC = '\033[0m'
 FAIL = '\033[91m'
@@ -190,7 +207,7 @@ class Search_Ambulance_Face(smach.StateMachine):
             
             with sm_conc:
                 # Search for face
-                smach.Concurrence.add('find_faces', recognize_face())
+                smach.Concurrence.add('find_faces', recognize_face_concurrent())
                 
                 smach.Concurrence.add('say_search_faces', say_searching_faces())
 
@@ -199,6 +216,94 @@ class Search_Ambulance_Face(smach.StateMachine):
                                 transitions={'succeeded':'succeeded', 
                                              'aborted':'aborted', 
                                              'preempted':'aborted'})
+
+
+######################################################################################################################
+
+class prepare_move_head(smach.State):
+    def __init__(self, head_position):
+        rospy.loginfo("Entering loop_test")
+        smach.State.__init__(self, outcomes=['succeeded','aborted', 'preempted'], 
+                                input_keys=['loop_iterations', 'head_left_right', 'head_up_down'],
+                                output_keys=['head_left_right', 'head_up_down', 'standard_error', 'loop_iterations'])
+        self.head_position = head_position
+
+    def execute(self, userdata):
+        
+        if userdata.loop_iterations == NUMBER_OF_HEAD_POSES:
+            userdata.loop_iterations = 0
+            return 'aborted'
+        else:
+            rospy.loginfo(userdata.loop_iterations)
+            userdata.standard_error='OK'
+            
+            userdata.head_left_right = 1 - userdata.loop_iterations*0.5
+            
+            if userdata.loop_iterations == 0:
+                userdata.head_left_right = 'mid_left'
+            elif userdata.loop_iterations == 1:
+                userdata.head_left_right = 'center'
+            elif userdata.loop_iterations == 2:
+                userdata.head_left_right = 'mid_right'
+
+            userdata.head_up_down = self.head_position if self.head_position else 'normal'
+
+            userdata.loop_iterations = userdata.loop_iterations + 1
+            return 'succeeded'
+
+class Search_Face_Determined(smach.StateMachine):
+    """
+        Search_Wave_SM: This SM searches for any face around the room, 
+                        as the robot will move its head from left to right.
+                        
+        Input Keys:
+            None
+        Output Keys:
+
+        Required Parameters: 
+            None
+            
+        Outcomes:
+
+    """
+    def __init__(self, head_position=None, text_for_wave_searching="I am looking for you."):
+        smach.StateMachine.__init__(self, outcomes=['succeeded', 'preempted', 'aborted'],
+                                 input_keys=[],
+                                 output_keys=['wave_position', 'wave_yaw_degree','standard_error'])
+        with self:
+            self.userdata.loop_iterations = 0
+            self.userdata.wave_position = None
+            self.userdata.wave_yaw_degree = None
+            self.userdata.standard_error = ''
+            smach.StateMachine.add(
+                                   'Move_head_prepare',
+                                   prepare_move_head(head_position),
+                                    transitions={'succeeded': 'move_head', 'aborted': 'aborted', 
+                                                'preempted': 'preempted'})
+            smach.StateMachine.add(
+                                   'move_head',
+                                   move_head_form(head_up_down=head_position),
+                                   transitions={'succeeded': 'Say_Searching',
+                                                'preempted':'Say_Searching',
+                                                'aborted':'aborted'})
+            smach.StateMachine.add(
+                'Say_Searching',
+                text_to_say(text_for_wave_searching),
+                transitions={'succeeded':'face_detection', 'aborted':'face_detection', 'preempted':'face_detection'})
+           
+            #Hard-coded maximum time in order to detect wave 
+            smach.StateMachine.add(
+                'face_detection',
+                detect_face(),
+                transitions={'succeeded': 'Say_Found', 'aborted': 'Move_head_prepare', 
+                'preempted': 'preempted'}) 
+            
+            smach.StateMachine.add(
+                'Say_Found',
+                text_to_say("Oh! I have found you finally."),
+                transitions={'succeeded':'succeeded', 'aborted':'aborted', 'preempted':'preempted'})
+            
+
 
                   
         
