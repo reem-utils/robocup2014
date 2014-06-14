6a67653d3df47e735a5a0751c1bc81873b7eef35
roger <roger@roger-K53SD.(none)>
2014-06-14 12:55:16 +0200

    forward node working

--- basic_states/navigation_states/src/navigation_states/forward_node.py
+++ basic_states/navigation_states/src/navigation_states/forward_node.py
@@ -10,16 +10,16 @@ import select
 
 from geometry_msgs.msg import Twist
 from get_current_robot_pose import get_current_robot_pose
-from navigation_states.srv import NavigationGoBack,NavigationGoBackResponse
+from navigation_states.srv import NavigationGoForward,NavigationGoForwardResponse
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Pose
 from util_states.math_utils import *
-from sensor_msgs.msg import Range # that is the ultrasound msgs
+from navigation_states.srv._NavigationGoForward import NavigationGoForward
 
-MAXMETERS=3 # this is the maximum numer of meters that can move the robot back
-MAXTIME=15 # numer maxim of time that the robot can be going back
+MAXMETERS=3 # this is the maximum numer of meters that can move the robot forward
+MAXTIME=15 # numer maxim of time that the robot can be going forward
 MINDIST=0.30
-SPEED_X=-0.3 # that is a back speed
+SPEED_X=0.3 # that is a forward speed
 MAXIM_INIT=200 # it'a a initialitzation value, it have to be bigger than ultraSounds Range
 NUM_MOSTRES=3
 
@@ -41,7 +41,7 @@ class navigation_back():
         rospy.loginfo("Initializing reverse")
         
         self.nav_pub= rospy.Publisher('/mobile_base_controller/cmd_vel', Twist)
-        self.nav_srv = rospy.Service('/reverse',NavigationGoBack, self.nav_back_srv)
+        self.nav_srv = rospy.Service('/forward',NavigationGoForward, self.nav_forward_srv)
         self.odom_subs = rospy.Subscriber("/mobile_base_controller/odom", Odometry, self.check_Odometry)
         self.init_var()
         
@@ -50,36 +50,23 @@ class navigation_back():
         self.Odometry_init=None
         self.enable=False
         
-        self.impacte=0
         self.goal_achieved=False
         
         self.time_init= rospy.get_rostime()
-        # ulta sound variables
-        self.sonar = [[MAXIM_INIT for y in xrange(NUM_MOSTRES)] for x in xrange(3)]
-        self.num_sonar=3*[0]
-        self.resultat=3*[MAXIM_INIT]
         
-        self.impacte=False
         self.time_out=False
         
         
-    def nav_back_srv(self,req):
+    def nav_forward_srv(self,req):
         
         if (req.meters<=MAXMETERS) :
             if (req.enable) :
                 
-		self.impacte=False
-       		self.time_out=False
-		self.sonar=[[MAXIM_INIT for y in xrange(NUM_MOSTRES)] for x in xrange(3)]
-        	self.num_sonar=3*[0]
-        	self.resultat=3*[MAXIM_INIT]
-		self.time_init= rospy.get_rostime()
-
-		self.ultra_subs=rospy.Subscriber("/sonar_base", Range, self.callback_Sonar)  
+                self.time_out=False
+                self.time_init= rospy.get_rostime()
                 self.enable=True
                 self.meters=req.meters#number of meters that we have to do
-                self.Odometry_init=self.Odometry_actual
-                
+                self.Odometry_init=self.Odometry_actual 
                 rospy.sleep(0.5)
                 self.run()
             else :
@@ -87,17 +74,15 @@ class navigation_back():
                 self.pub_stop()
             
             
-            if (self.time_out or self.impacte):
+            if (self.time_out):
                 rospy.loginfo("ABORTING!!!")
-            	rospy.loginfo(str(self.resultat))
-           	rospy.loginfo(str(self.sonar))
+                rospy.loginfo(str(self.resultat))
+                rospy.loginfo(str(self.sonar))
                 if self.time_out :
                     rospy.loginfo("TIME OUT")
-                if self.impacte :
-                    rospy.loginfo("IMPACT")
                 return "ABORTED"
             else :
-                return NavigationGoBackResponse()
+                return NavigationGoForwardResponse()
         else :
             self.pub_stop()
             return "Too much distance"
@@ -140,38 +125,6 @@ class navigation_back():
 
         self.nav_pub.publish(msg)
         
-    def callback_Sonar(self,data):
-       
-        timestamp = rospy.Time.now()
-        if (data.header.frame_id==('/base_sonar_07_link') or data.header.frame_id==('base_sonar_07_link')):
-                self.sonar[0][self.num_sonar[0]]=data.range
-                self.num_sonar[0]=self.num_sonar[0]+1
-                aux=sum(self.sonar[0])
-                self.resultat[0]=aux/NUM_MOSTRES
-                if self.num_sonar[0]>=NUM_MOSTRES :
-                    self.num_sonar[0]=0
-                
-        elif (data.header.frame_id==('/base_sonar_08_link' ) or data.header.frame_id==('base_sonar_08_link' )):
-                self.sonar[1][self.num_sonar[1]]=data.range
-                self.num_sonar[1]=self.num_sonar[1]+1
-                aux=sum(self.sonar[1])
-                self.resultat[1]=aux/NUM_MOSTRES
-                if self.num_sonar[1]>=NUM_MOSTRES :
-                    self.num_sonar[1]=0
-
-        elif (data.header.frame_id==('/base_sonar_09_link' ) or data.header.frame_id==('base_sonar_09_link' )):
-                self.sonar[2][self.num_sonar[2]]=data.range
-                self.num_sonar[2]=self.num_sonar[2]+1
-                aux=sum(self.sonar[2])
-                self.resultat[2]=aux/NUM_MOSTRES
-                if self.num_sonar[2]>=NUM_MOSTRES :
-                    self.num_sonar[2] =0
-        
-    def proces_ultraSound(self):	
-        if self.resultat[0]<MINDIST or self.resultat[1]<MINDIST or self.resultat[2]<MINDIST :
-            self.impacte= True
-        else :
-            self.impacte= False
             
             
     def proces_time(self):
@@ -190,14 +143,12 @@ class navigation_back():
            
             if self.enable: 
                 self.proces_odometry()
-                self.proces_ultraSound()
                 self.proces_time()
-                if not self.movment and not self.time_out and not self.impacte :
+                if not self.movment and not self.time_out :
                     self.pub_move()
                 else : 
                     self.pub_stop()
                     self.enable=False
-                    self.ultra_subs.unregister()
                     
                 rospy.sleep(0.01)
             else :
@@ -209,11 +160,12 @@ class navigation_back():
         
         
 if __name__ == '__main__':
-    rospy.init_node('navigation_reverse_service')
+    rospy.init_node('navigation_forward_service')
     rospy.sleep(1)
-    rospy.loginfo("navigation_reverse_srv")
+    rospy.loginfo("navigation_forward_srv")
     navigation = navigation_back()
     navigation.bucle()
     
     
+
   
