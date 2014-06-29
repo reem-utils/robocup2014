84b6370842efb7709a4cac477473193d779334e0
changlongzj <chlzr@hotmail.com>
2014-06-29 03:17:00 +0200

    Updates for move_head_to_point.py

--- basic_states/manipulation_states/src/manipulation_states/move_head_to_point.py
+++ basic_states/manipulation_states/src/manipulation_states/move_head_to_point.py
@@ -34,7 +34,7 @@ class prepare_move_head(smach.State):
         rospy.loginfo('Executing Move head')
         
         frame_id = self.s_frame_id if self.s_frame_id else userdata.target_frame_id
-        point_to_look = self.s_frame_id if self.s_frame_id else userdata.target_point 
+        point_to_look = self.s_point_to_look if self.s_point_to_look else userdata.target_point 
         
         look_goal = PointHeadGoal()
         look_goal.target.header.frame_id = frame_id
@@ -51,6 +51,8 @@ class prepare_move_head(smach.State):
             look_goal.pointing_axis.x = 1.0
             look_goal.pointing_axis.y = 0.0
             look_goal.pointing_axis.z = 0.0
+        
+        rospy.loginfo('Sending this goal: ' + str(look_goal))
         
         userdata.point_goal = look_goal
                
@@ -81,28 +83,44 @@ class move_head_to_point(smach.StateMachine):
         
     """
     def __init__(self, point_to_look=None, frame_id=None):
-        smach.StateMachine.__init__(self, outcomes=['succeeded', 'aborted', 'preempted'], 
-                                    input_keys=['move_head_pose'],
-                                    output_keys=['standard_error', 'move_joint_group','move_joint_poses', 'move_joint_list'])
+        smach.StateMachine.__init__(self, 
+                                    outcomes=['succeeded', 'aborted', 'preempted'], 
+                                    input_keys=['standard_error', 'target_frame_id', 'target_point'],
+                                    output_keys=['standard_error', 'point_goal'])
         with self:
             # Preparation of the SM
             smach.StateMachine.add('prepare_move_head',
-                                    prepare_move_head(), 
+                                    prepare_move_head(point_to_look, frame_id), 
                                     transitions={'succeeded':'send_look_to_point', 'preempted':'preempted', 'aborted':'aborted'})
 
             def point_head_result_cb(self, error, point_info):
                 print('Point Head Result: ' + str(point_info))
-                
+                print('ERROR: ' + str(error))
                 
             smach.StateMachine.add('send_look_to_point', 
                                    SimpleActionState(TOPIC_ACTION, 
                                                      PointHeadAction, 
                                                      goal_key='point_goal',
-                                                     exec_timeout=rospy.Duration(5.0), 
+                                                     exec_timeout=rospy.Duration(15.0), 
                                                      result_cb=point_head_result_cb, 
                                                      input_keys=['standard_error'], 
                                                      output_keys=['standard_error']),
                                    transitions={'succeeded':'succeeded', 'preempted':'preempted', 'aborted':'aborted'})
 
+def main(args):
+    rospy.init_node('look_to_point')
+    
+    sm = smach.StateMachine(outcomes=['succeeded', 'preempted', 'aborted', 'end_searching'])
+    with sm:
+        smach.StateMachine.add(
+            'gesture_state',
+            move_head_to_point(point_to_look=[1, 1, 0], frame_id='stereo_link'),
+            transitions={'succeeded': 'succeeded','preempted':'preempted', 'aborted':'aborted'})
 
+    sm.execute()
+    rospy.spin()
+    
+if __name__ == '__main__':
+    main(sys.argv)
+    
 
